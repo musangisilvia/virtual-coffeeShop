90e899dad8b5e1429b2f27615f88e5d266e849d0
Silvia Muthangya <musangisilvia@gmail.com>
2023-12-01 18:57:10 +0300

    add end point to test connection to db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 
 
 app = Flask(__name__)
 
 # configure db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:8QE1Oh8j9mPoruRmGtsw@database-1.creqgg4niwd7.eu-north-1.rds.amazonaws.com:3306/database-1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:8QE1Oh8j9mPoruRmGtsw@database-1.creqgg4niwd7.eu-north-1.rds.amazonaws.com:3306/VirtualCoffee'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 @app.route('/')
 def login():
-    return render_template('templates/index.html')
+    return render_template('index.html')
+
+@app.route('/test-db-connection')
+def test_db_connection():
+    try:
+        # Replace the values below with your actual database credentials
+        connection = mysql.connector.connect(
+            # :3306/VirtualCoffee
+            host="database-1.creqgg4niwd7.eu-north-1.rds.amazonaws.com",
+            user="admin",
+            password="8QE1Oh8j9mPoruRmGtsw",
+            database="VirtualCoffee"
+        )
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT VERSION()")
+        result = cursor.fetchone()
+
+        connection.close()
+
+        return jsonify({"status": "success", "result": result[0]}), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,5 @@
-username = admin
-password = '8QE1Oh8j9mPoruRmGtsw'
+-- username = admin
+-- password = '8QE1Oh8j9mPoruRmGtsw'
 
 CREATE DATABASE VirtualCoffee;
 
@@ -8,7 +8,7 @@ USE VirtualCoffee;
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    -- email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
